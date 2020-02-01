3041523d7e443181b26d54afe558ba63042fa8f9
ashwath19 <60543351+ashwath19@users.noreply.github.com>
2020-02-02 00:05:01 +1100

    First
    Commit

---
+++ precache-manifest.39016f8e42025bb4642a78a3dbcfe405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec2e9a96e28e2e267a9d88d06d749ca",
+    "url": "/templae-donations/index.html"
+  },
+  {
+    "revision": "3e579cc9ae058a7f42d7",
+    "url": "/templae-donations/static/css/main.02e55bee.chunk.css"
+  },
+  {
+    "revision": "67b94f32385d8d6baf77",
+    "url": "/templae-donations/static/js/2.92731de1.chunk.js"
+  },
+  {
+    "revision": "84f5adf97a6a466dd50e6b2b7291bba3",
+    "url": "/templae-donations/static/js/2.92731de1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e579cc9ae058a7f42d7",
+    "url": "/templae-donations/static/js/main.93d9961a.chunk.js"
+  },
+  {
+    "revision": "97e2041378b875d0c72f",
+    "url": "/templae-donations/static/js/runtime-main.7d57db8e.js"
+  }
+]);
